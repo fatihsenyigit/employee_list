48c5bfe14d4a0fe036a5c863a4b32bfe72d26b75
fatih <fsenyigit8@gmail.com>
2024-03-28 17:44:34 +0300

    List

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import List from './components/List';
+import React, { useState } from "react";
+import List from "./components/List";
+import data from "./helper/data";
 
 function App() {
- 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (currentPage < Math.ceil(data.length / 5)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const startIndex = (currentPage-1) * 5;
+  const currentPagePlayers = data.slice(startIndex, startIndex + 5)
+  console.log(currentPagePlayers)
   return (
     <main>
       <section className="container">
         <h3>Employee List</h3>
-        <h5>
-          (Employees 1 to 5)
-        </h5>
-        <List />
-        <div className='btns'>
-          <button >Prev</button>
-          <button >Next</button>
+        <h5>(Employees 1 to 5)</h5>
+        {currentPagePlayers.map((person)=>(
+          <List key={person.id} {...person}/>
+        ))}
+        
+        <div className="btns">
+          <button onClick={prevPage}>Prev</button>
+          <button onClick={nextPage}>Next</button>
         </div>
       </section>
     </main>
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-const List = () => {
+
+
+const List = ({image,name,email,age}) => {
   return (
     <>
       <article className="person">
-        <img src={"image"} alt={"name"} />
+        <img src={image} alt={name} />
         <div>
-          <h4>{"name"}</h4>
-          <p>{"email"}</p>
-          <p>{"age"} years</p>
+          <h4>{name}</h4>
+          <p>{email}</p>
+          <p>{age} years</p>
         </div>
       </article>
     </>
