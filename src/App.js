b8f32acb140cab9bab356c07091b4541f6a71cde
fatih <fsenyigit8@gmail.com>
2024-03-28 17:48:03 +0300

    button

--- src/App.js
+++ src/App.js
@@ -19,19 +19,25 @@ function App() {
 
   const startIndex = (currentPage-1) * 5;
   const currentPagePlayers = data.slice(startIndex, startIndex + 5)
-  console.log(currentPagePlayers)
   return (
     <main>
       <section className="container">
         <h3>Employee List</h3>
         <h5>(Employees 1 to 5)</h5>
-        {currentPagePlayers.map((person)=>(
-          <List key={person.id} {...person}/>
+        {currentPagePlayers.map((person) => (
+          <List key={person.id} {...person} />
         ))}
-        
+
         <div className="btns">
-          <button onClick={prevPage}>Prev</button>
-          <button onClick={nextPage}>Next</button>
+          <button disabled={currentPage === 1} onClick={prevPage}>
+            Prev
+          </button>
+          <button
+            disabled={currentPage === Math.ceil(data.length / 5)}
+            onClick={nextPage}
+          >
+            Next
+          </button>
         </div>
       </section>
     </main>
