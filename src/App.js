45ccc1fc1698b99ae613766910e0ed2b6f8c93ae
fatih <fsenyigit8@gmail.com>
2024-03-28 21:57:56 +0300

    data slice for current page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ import data from "./helper/data";
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
+  const startIndex = (currentPage - 1) * 5;
+  const currentPagePlayers = data.slice(startIndex, startIndex + 5);
+  const totalPages = Math.ceil(data.length / 5);
 
   const prevPage = () => {
-    const totalPages = Math.ceil(data.length / 5);
     const newPage = currentPage === 1 ? totalPages : currentPage -1;
     setCurrentPage(newPage);
   };
 
   const nextPage = () => {
-    const totalPages = Math.ceil(data.length/5)
     const newPage = currentPage === totalPages ? 1 : currentPage + 1;
     setCurrentPage(newPage)
   };
-
-  const startIndex = (currentPage-1) * 5;
-  const currentPagePlayers = data.slice(startIndex, startIndex + 5)
+  
   return (
     <main>
       <section className="container">
